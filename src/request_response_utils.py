db87c744e51f02aa3c37dfd376f6c494304bc325
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-17 23:49:33 -0500

    adding CORS

--- src/request_response_utils.py
+++ src/request_response_utils.py
@@ -21,6 +21,12 @@ def return_error_response(error_message, http_code):
 def return_status_ok(response_body):
     return {
         "statusCode": 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+            'content-type': 'application/json'
+        },
         "body": json.dumps(response_body),
     }
 
